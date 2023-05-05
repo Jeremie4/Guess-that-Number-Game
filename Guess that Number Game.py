85ec7102f6843e4269475dd541bfc4adbcac76e0
Jeremie4 <126706125+Jeremie4@users.noreply.github.com>
2023-05-05 10:58:39 -0600

    Updated code

--- Guess that Number Game.py
+++ Guess that Number Game.py
@@ -1,20 +1,22 @@
 import random
 
-try:
-    file = open("C:/Users/azzrrhk/Desktop/Python/Guess-that-Number-Game/high_scores.txt", "r")
-    high_scores = file.read()
-    file.close()
-    print("current high scores: \n" + high_scores)
-
-except:
-    print("Couldn't get high scores.")
-
-
 ran_num = 12 #random.randint(1,10)
 num_of_guesses = 0
-user_num = int(input("Welcome to the Guessing Game\nThe Computer has picked a number from 1 - 100. Try to match it.\nWhat number do you choose (1-100): "))
-high_scores_list = []
 
+
+try:
+    with open('C:/Users/azzrrhk/Desktop/Python/Guess-that-Number-Game/high_scores.txt', 'r') as f:
+        high_scores_list = [line.strip() for line in f]
+    
+    high_scores_list.remove("")
+    high_scores = high_scores_list
+
+except:
+    high_scores = "Couldn't get high scores."
+    high_scores_list = []
+
+
+user_num = int(input("Welcome to the Guessing Game\nHigh scores: " + str(high_scores) + "\nThe Computer has picked a number from 1 - 100. Try to match it.\nWhat number do you choose (1-100): "))
 
 while True:
 
@@ -33,27 +35,25 @@ while True:
 
 
 try:
-    file = open("C:/Users/azzrrhk/Desktop/Python/Guess-that-Number-Game/high_scores.txt", "r")
-    high_scores_list.append(int(file.read()))
+    if high_scores_list == []:
+        high_scores_list.append(num_of_guesses)
+    
+    else:
+        for item in high_scores_list:
+            if num_of_guesses < item:
+                high_scores_list.append(num_of_guesses)
+
     high_scores_list.sort()
-    file.close()
 
 except:
     pass
-
-
-try:
-    if num_of_guesses < int(high_scores_list[:10]):
-        high_scores_list.append(num_of_guesses)
-        high_scores_list.sort()
-
-except:
-    high_scores_list.append(num_of_guesses)
-    high_scores_list.sort()
+    #high_scores_list.append(num_of_guesses)
 
 file = open("C:/Users/azzrrhk/Desktop/Python/Guess-that-Number-Game/high_scores.txt", "w")
 file.close()
 file = open("C:/Users/azzrrhk/Desktop/Python/Guess-that-Number-Game/high_scores.txt", "a")
+
+#high_scores_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 for item in high_scores_list[:10]:
     file.write("\n" + str(item))
